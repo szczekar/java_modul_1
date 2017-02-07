0dfb126ac488c2c02bda7ee88fb1926ef263479e
Karol Szczepaniak <szczepaniakkarol@gmail.com>
2017-02-07 19:52:29 +0100

    Add files via upload

---
+++ First_program.java
@@ -0,0 +1,17 @@
+package pl.karolszczepaniak.podstawy_javy_modul_pierwszy;
+
+public class First_program {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is my first Java program which I can see at consola");
+		System.out.println("To jest liczba " + (5 + 5));
+		System.out.println("Cytuje teraz papie¿a \" Kocham Was Czule\" - koniec cytatu"); // cytaty
+
+		System.out.println("On powiedzia³ \'Witaj\' .");
+		System.out.println("On powiedzia³ \'Witaj karolu\' ");
+
+		System.out.println("\tWitaj z tabem "); // tak robimy taby !
+		System.out.println("\nWitaj w nowej linii ! "); // tak robimy now¹ linie
+	}
+}
