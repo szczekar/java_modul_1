687f10c17646c40c9accdf3489863ddbb1644adc
Karol Szczepaniak <szczepaniakkarol@gmail.com>
2017-02-20 20:50:00 +0100

    Add files via upload

---
+++ Drukuj.java
@@ -0,0 +1,11 @@
+package pl.modulpierwszy_java;
+
+public class Drukuj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wiedza to potega !");
+		System.out.println("Wiedza\nto\npotega !");
+		System.out.println(" ====================\n|Wiedza to potega ! |\n ====================");
+	}
+}
